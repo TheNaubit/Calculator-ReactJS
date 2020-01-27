feeeec26f625fb78fec02cfa4a9f7a2758311f5f
Alberto Del Águila <nautadev@gmail.com>
2020-01-27 18:43:01 +0100

    Revert "First commit!"
    This reverts commit d84ccb25c8bf0cceaeb8ea1cddf354b8eaec3a93.

--- README.md
+++ README.md
@@ -1,27 +1,28 @@
-# Weather Finde in ReactJS
-This project is the continuation of the tutorials I am writing for my upcoming blog teaching ReactJS. Meanwhile the blog is not finished yet (too much to write!), you can download this project and since it is full of comments, you will be ready for my tutorials once they are done!
+# ReactJS Project Structure
+This is a project structure I love using in my projects and since several of you asked to upload it to GitHub meanwhile you were doing my tutorials, here it is!
 
-## Is there a demo?
-Yeah, you can test this [here](#)!
+# How to use it?
+Easy! In assets folder just place the assets of your project, like images, fonts (you will need to create the fonts folder), sass and so on.
 
-## What features this project has?
-- ReactJS
-- Using async methods
-- Using git
-- Using API requests
-- Parsing API requests
-- CSS Animations
-- React Components
+In components just create a folder for each component and if a component has subcomponents, just create a component's folder inside.
 
-## How to install it?
+For styling the components I also use SCSS but you could use CSS if you prefer it!
+
+We have another folder called routes, where you will add all the routes of your app (in the project, the route's files are empty since this app has no routes).
+
+Utils folder is just for general functions you want to use in several places.
+
+And finally in the root folder (src) I only place the index.js file, the serviceWorker.js and the tests.
+
+# How to install it?
 Just run this code:
 ``` bash
-git clone https://github.com/NauCode/ReactJS-Project-Structure.git
+git clone https://github.com/NauCode/ReactJS-Project-Structure.git your_project_name
 ```
 
 After that, enter in the folder using:
 ``` bash
-cd .\Weather-Finder-ReactJS\
+cd .\your_project_name\
 ```
 
 And install the dependencies:
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,135 +1,26 @@
-import React from "react";
-// Let's import the weather component
-import Weather from "./../weather/weather";
+import React from 'react';
+import logo from '../../assets/images/logo.svg';
+import './app.scss';
 
-// And import the CloudsBackground component
-import CloudsBackground from "./../cloudsBackground/cloudsBackground";
-
-// And the component's style
-import "./app.scss";
-
-// Of course, import the Utils!
-import * as Utils from "./../../utils/functions";
-
-// This is the API key used to get the weather data
-const apiKey = "8ddceeacaf8b95fe943c88fc8389dee0";
-
-const Title = () => {
+function App() {
   return (
-    <div>
-      <h1 className="title-container__title">Weather Finder in ReactJS!</h1>
-      <h3 className="title-container__subtitle">
-        Find the current weather in any city!
-      </h3>
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <p>
+          Edit <code>src/app.js</code> and save to reload.
+        </p>
+        <a
+          className="app-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
-
-const Form = ({ onWeather }) => {
-  return (
-    <form onSubmit={e => onWeather(e)}>
-      <input type="text" name="city" placeholder="City..." />
-      <input type="text" name="country" placeholder="Country..." />
-      <button className="form-button">Get Weather!</button>
-    </form>
-  );
-};
-
-class App extends React.Component {
-  // We have to set an 'undefined' initial state
-  // meanwhile we get the weather's data
-  state = {
-    temperature: undefined,
-    city: undefined,
-    country: undefined,
-    humidity: undefined,
-    description: undefined,
-    error: undefined
-  };
-
-  // With this function we will send a request to the API
-  // and format the response to fit our state
-  // Using async!!
-  getWeather = async e => {
-    e.preventDefault();
-    const city = e.currentTarget.elements.city.value;
-    const country = e.currentTarget.elements.country.value;
-
-    // If a city and country have been specified by the user
-    if (city && country) {
-      try {
-        // Note: We have to use cors-anywhere since it enables
-        // cross-origin requests to anywhere
-        const apiCall = await fetch(
-          `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`
-        );
-
-        // We parse the response
-        const data = await apiCall.json();
-        const { main, sys, name, weather, timezone } = data;
-
-        const city_hour = Utils.getCityTimeWithTimezone(timezone).getHours();
-        console.log("Hour: "+city_hour);
-
-        console.log(data);
-        // And assign the parsed data to our state
-        this.setState({
-          temperature: main.temp,
-          city: name,
-          country: sys.country,
-          humidity: main.humidity,
-          description: weather[0].description,
-          error: ""
-        });
-      } catch (ex) {
-        console.log(ex.message);
-      }
-      // If no city and country have been specified by the user
-    } else {
-      // Just set our state data empty
-      this.setState({
-        temperature: undefined,
-        city: undefined,
-        country: undefined,
-        humidity: undefined,
-        description: undefined,
-        error: "please enter a valid values."
-      });
-    }
-  };
-
-  // Now let's code the render function for this component
-  render() {
-
-    // Remeber I use <> and </> to create an empty root element
-    return (
-      <>
-        <CloudsBackground />
-        <div className="wrapper">
-          <div className="main">
-            <div className="container">
-              <div className="row">
-                <div className="title-container">
-                  <Title />
-                </div>
-                <div className="form-container">
-                  <Form onWeather={this.getWeather} />
-                  <Weather
-                    temperature={this.state.temperature}
-                    city={this.state.city}
-                    country={this.state.country}
-                    humidity={this.state.humidity}
-                    description={this.state.description}
-                    error={this.state.error}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  }
 }
 
 export default App;
